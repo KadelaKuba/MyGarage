04cf58daef326b4d33d522a66f9f840c394c6d33
KadelaJakub <kadelakuba@gmail.com>
2018-12-09 15:11:27 +0100

    Create car detail

---
+++ app/src/main/java/com/example/kad0143/mygarageproject/CarDetailActivity.java
@@ -0,0 +1,84 @@
+package com.example.kad0143.mygarageproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CarDetailActivity extends Activity {
+
+    private TextView carBrandDetail;
+    private TextView carModelDetail;
+    private TextView carYearDetail;
+    private TextView carEngineDetail;
+    private ImageView carImageDetail;
+    private Car car;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_detail);
+
+        carBrandDetail = (TextView) findViewById(R.id.carBrandDetail);
+        carModelDetail = (TextView) findViewById(R.id.carModelDetail);
+        carYearDetail = (TextView) findViewById(R.id.carYearDetail);
+        carEngineDetail = (TextView) findViewById(R.id.carEngineDetail);
+        carImageDetail = (ImageView) findViewById(R.id.carImageDetail);
+
+        Long entryString = getIntent().getExtras().getLong("entryId");
+        if (!entryString.equals("")) {
+            getDataForCarFromDb(String.valueOf(entryString));
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+    private void getDataForCarFromDb(String entryString) {
+
+        DbHelper mDbHelper = new DbHelper(this);
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                CarTable._ID,
+                CarTable.COLUMN_NAME_BRAND,
+                CarTable.COLUMN_NAME_MODEL,
+                CarTable.COLUMN_NAME_YEAR,
+                CarTable.COLUMN_NAME_ENGINE,
+                CarTable.COLUMN_NAME_IMAGE
+        };
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                CarTable.COLUMN_NAME_BRAND + " ASC";
+        Cursor cursor = db.query(
+                CarTable.TABLE_NAME,
+                projection,
+                "_id = " + entryString,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(CarTable._ID));
+            String brand = cursor.getString(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_BRAND));
+            String model = cursor.getString(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_MODEL));
+            String year = cursor.getString(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_YEAR));
+            String engine = cursor.getString(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_ENGINE));
+            byte[] imgByte = cursor.getBlob(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_IMAGE));
+            car = new Car(itemId, brand, model, year, engine, DbBitmapUtility.getImage(imgByte));
+        }
+        cursor.close();
+
+        carBrandDetail.setText(car.brand);
+        carModelDetail.setText(car.model);
+        carYearDetail.setText(car.year);
+        carEngineDetail.setText(car.engine);
+        carImageDetail.setImageBitmap(car.image);
+    }
+}
--- app/src/main/java/com/example/kad0143/mygarageproject/MainActivity.java
+++ app/src/main/java/com/example/kad0143/mygarageproject/MainActivity.java
@@ -5,22 +5,73 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
     ListView lvEntries;
+    private ArrayList<Car> scoreRows;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        renderListView();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_cars);
 
+        final Gson gson = new Gson();
+        scoreRows = new ArrayList<Car>();
+
+        Button btnNewEntry = (Button) findViewById(R.id.addCarButton);
+        lvEntries = (ListView) findViewById(R.id.listView1);
+        renderListView();
+
+        btnNewEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewCar.class));
+            }
+        });
+
+        lvEntries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, CarDetailActivity.class);
+                long entryId = scoreRows.get(position).id;
+                intent.putExtra("entryId", entryId);
+                startActivity(intent);
+            }
+        });
+
+//        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+//        settingsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+//            }
+//        });
+    }
+
+    private void renderListView() {
+        getDataFromDB();
+        DataAdapter adapter = new DataAdapter(this, R.layout.list_entry_layout, scoreRows);
+        lvEntries.setAdapter(adapter);
+    }
+
+    private void getDataFromDB() {
         DbHelper mDbHelper = new DbHelper(this);
         // Gets the data repository in write mode
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
@@ -46,7 +97,7 @@ public class MainActivity extends Activity {
                 null,
                 sortOrder
         );
-        List<Car> scoreRows = new ArrayList<Car>();
+
         while (cursor.moveToNext()) {
             long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(CarTable._ID));
             String brand = cursor.getString(cursor.getColumnIndexOrThrow(CarTable.COLUMN_NAME_BRAND));
@@ -57,33 +108,5 @@ public class MainActivity extends Activity {
             scoreRows.add(new Car(itemId, brand, model, year, engine, DbBitmapUtility.getImage(imgByte)));
         }
         cursor.close();
-
-        lvEntries = (ListView) findViewById(R.id.listView1);
-        DataAdapter adapter = new DataAdapter(this, R.layout.list_entry_layout, scoreRows);
-        lvEntries.setAdapter(adapter);
-
-        Button btnNewEntry = (Button) findViewById(R.id.addCarButton);
-        btnNewEntry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, NewCar.class));
-            }
-        });
-
-//        Button settingsButton = (Button) findViewById(R.id.settingsButton);
-//        settingsButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-//            }
-//        });
-
-//        Button allCars = (Button) findViewById(R.id.allCars);
-//        allCars.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, ShowCarsActivity.class));
-//            }
-//        });
     }
 }
